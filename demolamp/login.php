a03045cc2eb93f4c1d113947e559662527d0700a
jailarie <47017619+jailarie@users.noreply.github.com>
2019-11-15 15:48:02 -0700

    Form Styling Updates

--- demolamp/login.php
+++ demolamp/login.php
@@ -34,11 +34,11 @@ echo <<<_END
             <h3>Enter username and password</h3>
         </div>
         <div data-role='fieldcontain'>
-            <label>Username</label>
+            <label>Username</label><br>
             <input type='text' maxlength='16' name='user' value='$user'>
         </div>
         <div data-role='fieldcontain'>
-            <label>Password</label>
+            <label>Password</label><br>
             <input type='password' maxlength='16' name='pass' value='$pass'>
         </div>
         <div data-role='fieldcontain'>
