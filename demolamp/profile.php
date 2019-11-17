c76490a57eb37dce987dcf5814344a345a5d144d
Joseph-Akers <jjg.akers@gmail.com>
2019-11-17 12:50:31 -0700

    added current book and book goal to profile page

--- demolamp/profile.php
+++ demolamp/profile.php
@@ -4,14 +4,21 @@
 if (!$loggedin) die("</div></body></html>");
 
 $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+    //$a = $result->fetch_array(MYSQLI_ASSOC);
+    //$book = $a[
+    //echo "<div> sometin </div>";
     
 if (isset($_POST['text'])) {
+    echo "<div> in if isset for textfield</div>";
     $text = sanitizeString($_POST['text']);
     $text = preg_replace('/\s\s+/', ' ', $text);
+    //echo "<div> text: $text </div>";
 
     if ($result->num_rows)
-         queryMysql("UPDATE profiles SET text='$text' where user='$user'");
-    else queryMysql("INSERT INTO profiles VALUES('$user', '$text')");
+        queryMysql("UPDATE profiles SET text='$text' where user='$user'");
+        //echo "<div> after query </div>";
+
+    else queryMysql("INSERT INTO profiles (user, text) VALUES('$user', '$text')");
 } 
 else {
     if ($result->num_rows) {
@@ -20,8 +27,84 @@ else {
     }
     else $text = "";
 }
-
 $text = stripslashes(preg_replace('/\s\s+/', ' ', $text));
+    
+// currently reading
+if (($_POST['currentBook']) != "") {
+    echo "<div> in if isset </div>";
+    $currentBook = sanitizeString($_POST['currentBook']);
+    $currentBook = preg_replace('/\s\s+/', ' ', $currentBook);
+
+    if ($result->num_rows)
+        //$row = $result->fetch_array(MYSQLI_ASSOC);
+        queryMysql("UPDATE profiles SET currentBookAuthor='$currentBook' where user='$user'");
+        //echo "<div> after query </div>";
+
+    else queryMysql("INSERT INTO profiles (user, currentBookAuthor) VALUES('$user', '$currentBook')");
+}
+else {
+    if ($result->num_rows) {
+        $row  = $result->fetch_array(MYSQLI_ASSOC);
+        $currentBook = stripslashes($row['currentBookAuthor']);
+        //$bookGoal = stripslashes($row['bookGoal']);
+
+        echo "<div> book: $currentBook </div>";
+    }
+    else $currentBook = "Enter A Book";
+}
+
+    //name='bookGoal'
+// reading goal
+if (($_POST['bookGoal']) != 0) {
+    echo "<div> in if isset bookGoal </div>";
+    $bookGoal = sanitizeString($_POST['bookGoal']);
+    $bookGoal = preg_replace('/\s\s+/', ' ', $bookGoal);
+
+    if ($result->num_rows)
+        //$row = $result->fetch_array(MYSQLI_ASSOC);
+        queryMysql("UPDATE profiles SET bookGoal='$bookGoal' where user='$user'");
+        //echo "<div> after query </div>";
+
+    else queryMysql("INSERT INTO profiles (user, bookGoal) VALUES('$user', '$bookGoal')");
+}
+else {
+    if ($result->num_rows) {
+        $row  = $result->fetch_array(MYSQLI_ASSOC);
+        $bookGoal = stripslashes($row['bookGoal']);
+        echo "<div> bookGoal: $bookGoal </div>";
+    }
+    else $bookGoal = 0;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    if (isset($_POST['text'])) {
+//        echo "<div> in if isset </div>";
+//        $text = sanitizeString($_POST['text']);
+//        $text = preg_replace('/\s\s+/', ' ', $text);
+//        echo "<div> text: $text </div>";
+//
+//        if ($result->num_rows)
+//            queryMysql("UPDATE profiles SET text='$text' where user='$user'");
+//            //echo "<div> after query </div>";
+//
+//        else queryMysql("INSERT INTO profiles (user, text) VALUES('$user', '$text')");
+//    }
+//    else {
+//        if ($result->num_rows) {
+//            $row  = $result->fetch_array(MYSQLI_ASSOC);
+//            $text = stripslashes($row['text']);
+//        }
+//        else $text = "";
+//    }
+//
+//$text = stripslashes(preg_replace('/\s\s+/', ' ', $text));
 
 if (isset($_FILES['image']['name'])) {
     $saveto = "userpics/$user.jpg";
@@ -68,6 +151,10 @@ echo <<<_END
     <form method='post' action='profile.php' enctype='multipart/form-data'>
         <h3>Enter or edit your bio</h3>
         <textarea name='text'>$text</textarea><br>
+        <label for='currentBook'>Currently Reading:</label>
+        <input type='text' name='currentBook' placeholder='$currentBook'><br>
+        <label for='bookGoal'>My Reading Goal:</label>
+        <input type='text' name='bookGoal' placeholder='$bookGoal'> <br>
         <h3>Upload an image</h3>
         Image: <br>
         <input type='file' name='image' size='14'><br><br>
@@ -81,3 +168,7 @@ showProfile($user);
 
 require_once 'footer.php';
 ?>
+
+
+
+
